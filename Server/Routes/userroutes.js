1e66af7c7d5e36d5c10467701e1da096ccb15fa0
Kaustubh Patil <117383257+Anonymouskau@users.noreply.github.com>
2023-01-19 17:58:58 +0530

    Add files via upload

---
+++ Server/Routes/userroutes.js
@@ -0,0 +1,62 @@
+    import { Router } from "express";
+
+import User from "../models/User.js";
+
+const route=Router()
+
+
+route.post('/user', async (req,res)=>{
+
+ 
+    try {
+        
+            
+                 
+        var User1=new User({
+            name:req.body.name,
+             email:req.body.email,
+         password:req.body.password,
+        phoneno:req.body.phoneno,
+           role:req.body.role    
+        })
+       await User1.save().then(()=>res.send("Sucsess")).catch(err=>console.log(err))
+        } catch (error) {
+        console.log(error)
+        
+      }
+
+      }  )
+
+
+route.get('/user',(req,res)=>{
+
+  User.find().then((user)=>res.send(res.json(user))).catch(err=>console.log(err))
+
+
+
+
+
+
+
+})
+
+
+route.put('/forgotpass',(req,res)=>{
+   
+   const emailbody=req.body.email
+    const passbody=req.body.password
+    
+    User.find({email:emailbody}).then(email=>console.log(res.json(email))).catch(err=>console.log(err))
+
+
+    
+})
+
+route.get('/remove',(req,res)=>{
+
+
+    
+})
+
+
+export default (route)
