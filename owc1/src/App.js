df09bca7b6bb744a6a09c1a6ae16897e836fe1ff
Kaustubh Patil <117383257+Anonymouskau@users.noreply.github.com>
2023-01-24 16:33:34 +0530

    Add files via upload

---
+++ owc1/src/App.js
@@ -0,0 +1,15 @@
+import SignIn from './Frontend/Navbar';
+import './App.css';
+import Footer from './Frontend/footer';
+import Slider from './Frontend/slider/slider';
+function App() {
+  return (
+    <>
+  <SignIn></SignIn>
+  <Slider></Slider>
+  <Footer></Footer>
+   </>
+  );
+}
+
+export default App;
