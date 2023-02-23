45c7022f9029d7118771d94845194b7ec0bd374e
unknown <kaustubhpatil418@gmail.com>
2023-02-23 15:47:50 +0530

    23-2-2023

--- owc/src/Frontend/Displaypage/Education.js
+++ owc/src/Frontend/Displaypage/Education.js
@@ -23,8 +23,9 @@ export default function Properties() {
     return (
     <>
      &nbsp;
-    <h1>CLASS-ROOMS</h1>
-
+     <h1 style={{"margin":"4%","fontFamily":"italic"}}> CLASS ROOMS</h1>
+     <hr style={{"color":"black"}}></hr>
+       
         
         
     <iframe src={"https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3867250.9286021274!2d76.77476745!3d18.815426549999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1677051620764!5m2!1sen!2sin"} width="800" height="1000" style={{"border":"0" ,"allowfullscreen":"" ,"loading":"lazy" ,"referrerpolicy":"no-referrer-when-downgrade",} }align="right"></iframe>  
