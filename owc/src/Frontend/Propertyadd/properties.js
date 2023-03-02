c0a434cfa10598a1a1414d359ec7d2253cd560d9
unknown <kaustubhpatil418@gmail.com>
2023-03-02 16:10:19 +0530

    Kpcommit

--- owc/src/Frontend/Propertyadd/properties.js
+++ owc/src/Frontend/Propertyadd/properties.js
@@ -1,33 +1,90 @@
-import React from 'react'
+// Property1 = new Property({
+//   country: req.body.country,
+//   State: req.body.State,
+
+//   city: req.body.city,
+
+//   Propertyname: req.body.Propertyname,
+//   Status: req.body.Status,
+//   Propertydesc: req.body.Propertydesc,
+//   propertytype: req.body.propertytype,
+//   subtype: req.body.subtype,
+//   prize: req.body.prize,
+
+//   image: req.body.image,
+
+import React, { useState } from 'react'
 import '../Propertyadd/padd.css'
-export  default function properties  ()  {
+export  default function Properties  ()  {
+  
+
+  const [state,setstate]=useState({
+     
+    country:"",
+    State:"",
+    city:"",
+    Propertyname:"",
+    Status:"",
+    Propertydesc:"",
+    propertytype:"",
+    subtype:"",
+    prize:"",
+    image:""
+  })
+  const handler=(e)=>{
+     const{name,...value}= e.target.value
+     console.log(name);
+     console.log(value);
+    setstate({
+      ...state,[name]:value
+    })
+    
+  
+  }
+  
   return (
     <>
     &nbsp;
     <div class='container1'>
     <div class="container1">  
   <form id="contact" action="" method="post">
-    <h3>Colorlib Contact Form</h3>
-    <h4>Contact us for custom quote</h4>
+    <h3> Add Properties</h3>
+    <h4>Enter each details Properly !!! </h4>
     <fieldset>
-      <input placeholder="Your name" type="text" tabindex="1" required autofocus />
+      <input onChange={handler} defaultValue={state.country} name="country" placeholder="Country" type="text" tabindex="1" required autoFocus />
     </fieldset>
     <fieldset>
-      <input placeholder="Your Email Address" type="email" tabindex="2" required />
+      <input onChange={handler} defaultValue={state.State} name='State' placeholder="State" type="text" tabindex="2" required />
     </fieldset>
     <fieldset>
-      <input placeholder="Your Phone Number (optional)" type="tel" tabindex="3" required />
+      <input  onChange={handler} defaultValue={state.city}name='city' placeholder="City" type="text" tabindex="3" required />
     </fieldset>
     <fieldset>
-      <input placeholder="Your Web Site (optional)" type="url" tabindex="4" required />
+      <input  onChange={handler} defaultValue={state.Propertyname} name='Propertyname' placeholder="Propertyname" type="text" tabindex="4" required />
     </fieldset>
     <fieldset>
-      <textarea placeholder="Type your message here...." tabindex="5" required></textarea>
+      
+    <fieldset>
+      <input onChange={handler} defaultValue={state.prize} name='prize'placeholder="Prize" type="text" tabindex="4" required />
+    </fieldset>
+
+    <fieldset>
+      <input  onChange={handler} defaultValue={state.Status}name='Status' placeholder="Status" type="text" tabindex="4" required />
     </fieldset>
     <fieldset>
-      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+      <input onChange={handler} defaultValue={state.image} name='image' placeholder="Image link use Links from pexels" type="url" tabindex="4" required />
     </fieldset>
-    <p class="copyright">Designed by <a href="https://colorlib.com" target="_blank" title="Colorlib">Colorlib</a></p>
+      <textarea  defaultValue={state.Propertydesc} name="Propertydesc"placeholder="Property desc" tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button onClick={()=>{
+
+
+
+
+      }} name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+    <p class="copyright"> <a href="https://colorlib.com" target="_blank" title="Colorlib"></a></p>
   </form>
 </div>
     
